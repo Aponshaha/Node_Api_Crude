4f1d832d29c540365f724e85f71ef14e6d67c0bf
Apon Saha <aponsaha@Apons-MacBook-Pro.local>
2020-04-15 15:43:03 +0900

    commited

--- Controllers/Product.Controller.js
+++ Controllers/Product.Controller.js
@@ -7,9 +7,10 @@ module.exports = {
   getAllProducts: async (req, res, next) => {
     try {
       const results = await Product.find({}, { __v: 0 });
-      // const results = await Product.find({}, { name: 1, price: 1, _id: 0 });
-      // const results = await Product.find({ price: 699 }, {});
+        // const results = await Product.find({}, { name: 1, price: 1, _id: 0 });
+        // const results = await Product.find({ price: 699 }, {});
       res.send(results);
+      
     } catch (error) {
       console.log(error.message);
     }
--- app.js
+++ app.js
@@ -38,5 +38,5 @@ app.use((err, req, res, next) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log('Server started on port ' + PORT + '...');
+  console.log('Server started on port ' + PORT + 'on DB' );
 });
